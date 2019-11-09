22ee2d8a90d6331e6c118e73bf917e428188f63a
Andrea Roveroni <roveroniandrea@gmail.com>
2019-11-09 14:58:29 +0100

    HTPLCompiler v1.0

--- README.md
+++ README.md
@@ -1,2 +1,278 @@
-# HTPLcompiler
-HTPL is a programming language based on HTML
+# HTPL Compiler by Andrea Roveroni
+It's known that HTML is not a programming language. In fact it stands for Hyper Text Markup Language.
+
+No one could make a program in HTML.
+
+**UNTIL NOW**
+
+Welcome to the HTPLcompiler, a compiler that lets you program in HTML.
+
+Ladies and gentleman, ~~I'm~~ we're proud to present you the **HTPL**, or **Hyper Text Programming Language**.
+
+The compiler checks for all elements inside a defined element (which is `<HTPL>`) in body, and creates a `<script>` element containg JS code.
+
+#
+## What can you do in HTPL?
+HTPL supports lots of the basic feature required for a programming language. It allows you to declare variables, compute operations, assign values, evaluating logical expressions, inserting conditions with an `if then else` structure, and supports the `while` loop construct. In addistion to this, it provides interfaces for user inputs and program outputs via `prompt` and `alert` JS functions.
+
+#
+## Why using HTPL?
+HTPL main purpose is to destoy the "HTML is not a programming language" meme, but you're able to make complex programs too, if you want.
+
+#
+## Get started
+If you want to simply inject the compiler in your code, download `main.js` file and attach it in you `index.html` file. If you need some examples and introduction, continue reading.
+
+HTPLCompiler interface can be found on your browser console, and provides you with an help() function to get started.
+
+#
+## HTPL tag
+`<HTPL>` tag must contain all the HTPL code you want to compile. Children elements must respect the rules of HTPL programming language. All elements that are not children of HTPL tag are simply ignored.
+
+## Variable declaration
+Variables can be declared with the following syntax:
+```html
+<h1 id="varName">
+    <p>A value</p>
+</h1>
+```
+which is compiled as
+```js
+let varName = 'A value';
+```
+More details about the `<p>` element in the next section
+
+## Returning values
+The `<p>` element is used to return string values, integer values or variable values. This code:
+```html
+<p>A string value</p>
+<p id="10"></p>
+<p id="varname"></p>
+```
+is compiled as
+```js
+'A string value'
+10
+varname
+```
+Please note that a string value will be returned only if no id is specified. For example:
+```html
+<p id="foo">A string value</p>
+```
+will return
+```js
+foo
+```
+
+## Operations between values
+The `<b>` element is used to execute operations between two values. The operation is defined by its id, for example:
+```html
+<b id="+">
+    <p id="10"></p>
+    <p id="foo"></p>
+</b>
+<b id="*">
+    <p id="10"></p>
+    <p id="foo"></p>
+</b>
+```
+is compiled as
+```js
+10 + foo
+10 * foo
+```
+
+## Value assignment
+The `<h2>` element is used to assign a new value to an existing variable, specified by the id. For example:
+```html
+<h2 id="foo">
+    <b id="+">
+        <p id="10"></p>
+        <p id="foo"></p>
+    </b>
+</h2>
+```
+is compiled as
+```js
+foo = 10 + foo
+```
+Please note that `foo` must be previously declared with `<h1>` element. If foo is not previously declared, the compiler won't give you an error, but the JS code will not work.
+
+## If then else branches
+The if then else is defined by the `ul` element. This element must have exactly two or three branches (condition, if branch and optional else branch). This code:
+```html
+<ul>
+    <strong id="==">
+        <p id="foo"></p>
+        <p id="10"></p>
+    </strong>
+    <if>
+        <p>Some code here</p>
+    </if>
+    <else>
+        <p>Some other code here</p>
+    </else>
+</ul>
+```
+is compiled as:
+```js
+if(foo == 10){
+    'Some code here';
+}
+else{
+    'Some other code here';
+}
+```
+
+The element `<strong>` will be explained in details later, but it stands for a comparison. Please note that condition, if and else branches must be declared in this exact order.
+
+## Comparison and logical operations
+As seen previously, a comperison is identified by the `<strong>` element. The operator is specified by the id. Exactly two children are required, both of them must return a comparable value:
+```html
+<strong id=">=">
+    <p id="foo"></p>
+    <p id="10"></p>
+</strong>
+<strong id="!=">
+    <p id="foo"></p>
+    <p>Hello there!</p>
+</strong>
+```
+is compiled as:
+```js
+foo >= 10;
+foo != 'Hello there!';
+```
+`<strong>` comparison can be chained with `<and>`, `<or>` and `<not>` logical operations to make more complex conditions. Both `<and>`, `<or>` require exactly two children, while `<not>` requires only one child:
+```html
+<or>
+    <and>
+        <strong id=">=">
+            <p id="foo"></p>
+            <p id="10"></p>
+        </strong>
+        <strong id="==">
+            <p id="phrase"></p>
+            <p>Hello there!</p>
+        </strong>
+    </and>
+    <not>
+        <strong id="==">
+            <p id="reply"></p>
+            <p>General Kenobi</p>
+        </strong>
+    </not>
+</or>
+```
+is compiled as:
+```js
+((foo >= 10) && (phrase == 'Hello there!') || (!(reply == 'General Kenobi')))
+```
+This comparisons can be inserted as first child of `<ul>` elements to create complex if conditions
+
+#
+## Function declaration and invocation
+A function can be declared with the `<div>` element. Function name is specified by the id, and optional parameters are specified by the classes of the element:
+```html
+<div id="anakinSays" class="name, age">
+    <return>
+        <p>This is were the fun begins</p>
+    </return>
+</div>
+```
+this is compiled as:
+```js
+function anakinSays(name, age){
+    return 'This is were the fun begins';
+}
+```
+Classes can be omitted if function does not require any parameters. Also `<return>` can be omitted to make a void function.
+
+Functions can be invoked with `<h3>` tag. Function name must be specified with the id (without brackets) and all the children are considered parameters:
+```html
+<h3 id="anakinSays">
+    <p>Andrea</p>
+    <p id="19"></p>
+</h3>
+```
+is compiled as:
+```js
+anakinSays('Andrea', 19);
+```
+
+#
+## While loop
+This is the only available loop in HTPL. It's defined by `<while>` element (so much fantasy). At least two children are required, the first for the condition and the second (and others) for the code to loop:
+```html
+<while>
+    <strong id=">">
+        <p id="numberOfLoops"></p>
+        <p id="0"></p>
+    </strong>
+    <h2 id="numberOfLoops">
+        <b id="-">
+            <p id="numberOfLoops"></p>
+            <p id="1"></p>
+        </b>
+    </h2>
+</while>
+```
+this code is compiled as:
+```js
+while(numberOfLoops > 0){
+    numberOfLoops = numberOfLoops - 1;
+}
+```
+
+#
+## Inputs and outputs
+If you want to ask the user to insert a value, you have to use `<prompt>` element. The optional message is specified by the id:
+```html
+<h2 id="foo">
+    <prompt id="Please insert a value for foo: ">
+    </prompt>
+</h2>
+```
+this code is compiled as:
+```js
+foo = prompt('Please insert a value for foo: ');
+```
+Outputs are displayed with `<cite>` element:
+```html
+<cite>
+    <b id="+">
+        <p>Foo now has values: </p>
+        <p id="foo"></p>
+    </b>
+</cite>
+```
+Which is compiled as an `alert()` function:
+```js
+alert(('Foo has new values: ' + foo))
+```
+
+#
+## Comments
+All elements that are not children of the `<HTPL>` tag will be ignored. If you want to add comment to your code, you can use the traditional HTML comment `<!--comment-->` or assign the class `HTPL-ignore` to the element. This makes the compiler to skip the element and all his children:
+```html
+<!--This cite tag and all his children will be ignored-->
+<cite class="HTPL-ignore">
+    <b id="+">
+        <p>Foo now has values: </p>
+        <p id="foo"></p>
+    </b>
+</cite>
+```
+
+# HTPLCompiler interface
+The HTPLCompiler interface can be accessed by opening che debugger's console in your browser. You will see a welcome log.
+The interface is implemented in one static class `HTPLCompiler`. His two most important functions are `HTPLCompiler.compile(displayCode = false)`, which compiles your HTPL code and creates a `<script>` element, and `HTPLCompiler.runHTPL()`, that runs your compiled script tag. You may execute both of them by calling `HTPLCompiler.compileAndRun(displayCode = false)`. If you call the compiler by passing true as the argument, the compiled JS code will be logged in the console.
+
+If you need help, type `HTPLCompiler.help()` on your console.
+
+## Error handling
+During compilation, the HTPLCompiler will detect the following errors:
+- Missing or incorrect id on element (where required)
+- Wrong number of children elements in constructs (where an exact number is required)
+
+Please note that **the HTPLCompiler will not validate your code**, check for undecrared variables, misspelling and so on. This work is handled by the javascript engine in your browser when you run your code.
---
+++ main.js
@@ -0,0 +1,269 @@
+window.addEventListener('load', () => HTPLCompiler.welcome());
+
+class HTPLCompiler {
+
+    static welcome(){
+        console.log(`HTPLCompiler enabled
+        To begin running code, type HTPLCompiler.compileAndRun()
+        To display help menu, please type HTPLCompiler.help()
+
+        HTPLCompiler by Andrea Roveroni
+        `);
+    }
+
+    static help(){
+        console.log(`List of available commands:
+        compile() -> Compiles HTPL code and attaches a script element on body
+        runHTPL() -> Runs script attacked to body. HTML code must have been previously compiled
+        compileAndRun() -> Calls both compile() and runHTPL()
+        compileElement() -> Compiles a single element and his children and returns the code (does not create script element)
+        compileChildOf() -> Compiles the cildren of an element but not the element itself. Returns the code (does not create script element)
+        help() -> Displays a list of available commands
+        helpHTPL() -> Displays information on how to program in HTPL
+        welcome() -> Displays welcome information`);
+    }
+
+    static helpHTPL(){
+        console.log('Section not available');
+    }
+
+    static compile(displayCode = false) {
+        //HTML element containg code
+        let HTPLelement = document.querySelector('HTPL');
+
+        if(!HTPLelement){
+            throw `Error, HTPL element not found. Please create HTPL element in order to run HTML`
+        }
+        //Compiled HTPL code
+        let stringCompiled = this.compileElement(HTPLelement);
+        if(!stringCompiled){
+            throw `Error, no code compiled but HTPL element was found. Check for HTPL-ignore class on element`
+        }
+        if(displayCode){
+            console.log('HTPL compiled!', stringCompiled);
+        }
+
+        //add or replace script element to run HTPL
+        let scriptElement = document.querySelector('#HTPLScript');
+        if (scriptElement) { document.removeChild(scriptElement) };
+        scriptElement = document.createElement('script');
+        scriptElement.innerHTML = stringCompiled;
+        document.body.appendChild(scriptElement);
+
+        console.log('Script tag added in body element. Please run HTPLCompiler.runHTPL() to execute code')
+    }
+
+    static compileAndRun(displayCode = false) {
+        this.compile(displayCode);
+        this.runHTPL();
+    }
+
+    static runHTPL() {
+        let scriptElement = document.querySelector('#HTPLScript');
+        if(!scriptElement){
+            throw 'No HTPLScript found in body, please compile HTPL before running it';
+        }
+        compiledCode();
+    }
+
+    static compileElement(element) {
+        let resultString = '';
+        if (!element || !element.tagName || element.classList.contains('HTPL-ignore')) return resultString;
+        switch (element.tagName.toLowerCase()) {
+            //start of HTPL code
+            case 'htpl': {
+                resultString = `function compiledCode(){
+                    ${this.compileChildOf(element)}
+                }`
+                break;
+            }
+            //declaration with assignment
+            case 'h1': {
+                let id = element.id;
+                if (!id) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): variable declaration must provide variable name (missing id)`;
+                }
+                resultString = `let ${id} = ${this.compileElement(element.children[0])}`;
+                break;
+            }
+            //operation between two values
+            case 'b': {
+                if(!element.id){
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): operation must define operation type (missing id)`;
+                }
+                if (element.childElementCount != 2) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): sum must have two operators (wrong numer of children)`;
+                }
+                else {
+                    resultString = `(${this.compileElement(element.children[0])} ${element.id} ${this.compileElement(element.children[1])})`;
+                }
+                break;
+            }
+            //value or variable name
+            case 'p': {
+                if (element.id) {
+                    resultString = `${element.id}`;
+                }
+                else
+                    resultString = `'${element.innerHTML}'`
+                break;
+            }
+            //assignment
+            case 'h2': {
+                let id = element.id;
+                if (!id) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): variable assignment must provide variable name (missing id)`;
+                }
+                resultString = `${id} = ${this.compileElement(element.children[0])};`;
+                break;
+            }
+            //alert
+            case 'cite': {
+                resultString = `alert(${this.compileElement(element.children[0])})`;
+                break;
+            }
+            //condition
+            case 'ul': {
+                if (element.childElementCount != 2 && element.childElementCount != 3) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): if/else construct must have two or three operators (wrong number of children)`;
+                }
+                resultString = `if(${this.compileElement(element.children[0])}){
+                    ${this.compileElement(element.children[1])}
+                }
+                else {
+                    ${this.compileElement(element.children[2])}
+                }`
+                break;
+            }
+            //comparison == >= <= !=
+            case 'strong': {
+                if (!element.id || (element.id != '==' && element.id != '!=' && element.id != '<=' && element.id != '>=' && element.id != '<' &&  element.id != '>' &&  element.id != '===')) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): Comparison operations must define a valid comparison type (missing or incorrect id)`;
+                }
+                if (element.childElementCount != 2) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): Comparison operation ${element.id} must have two operators (wrong number of children)`;
+                }
+                resultString = `(${this.compileElement(element.children[0])} ${element.id} ${this.compileElement(element.children[1])})`
+                break;
+            }
+            //and
+            case 'and': {
+                if (element.childElementCount != 2) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): AND condition must have two operators (wrong number of children)`;
+                }
+                resultString = `(${this.compileElement(element.children[0])} && ${this.compileElement(element.children[1])})`
+                break;
+            }
+            //or
+            case 'or': {
+                if (element.childElementCount != 2) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): OR condition must have two operators (wrong number of children)`;
+                }
+                resultString = `(${this.compileElement(element.children[0])} || ${this.compileElement(element.children[1])})`
+                break;
+            }
+            //not
+            case 'not': {
+                if (element.childElementCount != 1) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): NOT condition must have one operator (wrong number of children)`;
+                }
+                resultString = `!(${this.compileElement(element.children[0])})}`
+                break;
+            }
+            //if branch
+            case 'if': {
+                resultString = `${this.compileChildOf(element)}`;
+                break;
+            }
+            //else branch
+            case 'else': {
+                resultString = `${this.compileChildOf(element)}`;
+                break;
+            }
+            //prompt branch
+            case 'prompt': {
+                resultString = `prompt('${element.id}')`;
+                break;
+            }
+
+            //function declaration
+            case 'div': {
+                let id = element.id;
+                if (!id) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): function declaration must provide function name (missing id)`;
+                }
+                let params = element.classList.toString();
+                resultString = `function ${id} (${params}){
+                    ${this.compileChildOf(element)}
+                }`;
+                break;
+            }
+            //function invocation
+            case 'h3': {
+                if (!element.id) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): function invocation must provide function name (missing id)`;
+                }
+                resultString = `${element.id}(`
+                for (let i = 0; i < element.childElementCount; i++) {
+                    resultString += `${this.compileElement(element.children[i])}`;
+                    if (i != element.childElementCount - 1) {
+                        resultString += `, `;
+                    }
+                }
+                resultString += `)`;
+                break;
+            }
+            //function return
+            case 'return' : {
+                if (element.childElementCount != 1) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): Return statemant must have only one child (wrong number of children)`;
+                }
+                resultString = `return ${this.compileElement(element.children[0])}`;
+                break;
+            }
+            //while loop
+            case 'while':{
+                if (element.childElementCount < 2) {
+                    console.error('Error in: ', element);
+                    throw `Error in ${element} (see above): while loop must have at least one code and one condition branches (wrong number of children)`;
+                }
+                resultString = `while(${this.compileElement(element.children[0])}){
+                    `;
+                for(let i= 1; i< element.childElementCount; i++){
+                    resultString += `${this.compileElement(element.children[i])};
+                    `;
+                }
+                resultString += `}`;
+                break;
+            }
+            default: {
+                console.warn('Warning, element not recognized, code skipped', element);
+                resultString = ``;
+                break;
+            }
+        }
+        return resultString;
+    }
+
+    static compileChildOf(element) {
+        let resultString = ``;
+        for (let i = 0; i < element.childElementCount; i++) {
+            resultString += `${this.compileElement(element.children[i])};
+            `;
+        }
+        return resultString;
+    }
+}
